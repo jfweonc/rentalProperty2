682f38d77fdf2456510917779788eab264afbabf
Guolong Li <Guolong.LiRealtor@gmail.com>
2022-12-30 22:47:19 -0600

    rentalPropertyAnalysisMVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentalPropertyAnalysisMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var repo = new OfferRepository();
+            var offers = repo.GetAllOffers();
+            return View(offers);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
